53a1ac1bf1511348ee8daa30aca326aa261a4e81
nerooon123 <keleron123@gmail.com>
2023-05-20 21:38:27 +0300

    Добавьте файлы проекта.

---
+++ YourHealthTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("YourHealthTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("YourHealthTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("35ef9ebe-5b33-47b9-93ce-d7c86efffe34")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ YourHealthTests/YourHealthUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace YourHealthTests
+{
+    [TestClass]
+    public class YourHealthUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
